881bdaffa1cb3feb92b2ca435af81b07456a322e
redovic <dnl.rammer@gmail.com>
2018-08-03 23:32:16 +0200

    added project files

---
+++ Assets/Scripts/DroneGenerator.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DroneGenerator : MonoBehaviour {
+    private bool heightIsFixed = false;
+    private List<Follower> drones;
+
+    public static int COLUMS = 0;
+    public static int ROWS = 0;
+
+    public GameObject prefab;
+    public int colums = 10;
+    public int rows = 10;
+    public float offset = 1.5f;
+
+    public Vector3 startPos;
+    Vector3 nextPos;
+
+	void Start () {
+
+        drones = new List<Follower>();
+        if (COLUMS != 0) {
+            colums = COLUMS;
+        } else {
+            COLUMS = colums;
+        }
+        if (ROWS != 0) {
+            rows = ROWS;
+        } else {
+            ROWS = rows;
+        }
+
+        startPos = new Vector3(-colums / 2 * offset, 0, rows / 2 * offset);
+
+		for (int c = 0; c < colums; c++) {
+            for (int r = 0; r < rows; r++) {
+                drones.Add(Instantiate(prefab, startPos + nextPos, Quaternion.identity).GetComponent<Follower>());
+                nextPos.z -= offset;
+            }
+
+            nextPos.z = 0;
+            nextPos.x += offset;
+        }
+	}
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.Alpha1)) {
+            Restart(1, 1);
+        }
+        if (Input.GetKey(KeyCode.Alpha2)) {
+            Restart(2, 2);
+        }
+        if (Input.GetKey(KeyCode.Alpha3)) {
+            Restart(5, 5);
+        }
+        if (Input.GetKey(KeyCode.Alpha4)) {
+            Restart(10, 10);
+        }
+        if (Input.GetKey(KeyCode.Alpha5)) {
+            Restart(20, 20);
+        }
+        if (Input.GetKey(KeyCode.Alpha6)) {
+            Restart(40, 10);
+        }
+        if (Input.GetKey(KeyCode.Alpha7)) {
+            Restart(25, 20);
+        }
+        if (Input.GetKeyDown(KeyCode.T)) {
+            foreach (Follower item in drones) {
+                item.EnablePathDrawing(!item.drawPath);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.G)) {
+            foreach (Follower item in drones) {
+                item.EnableSensorRangeDrawing(!item.showSensor);
+            }
+        }
+        //if (Input.GetKeyDown(KeyCode.ASDASD)) {
+        //    heightIsFixed = !heightIsFixed;
+        //    foreach (Follower item in drones)
+        //        item.FixHeight(!heightIsFixed);
+        //}
+    }
+
+    private void Restart(int c, int r) {
+        COLUMS = c;
+        ROWS = r;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour {
+    [SerializeField]
+    private float currentSpeed;
+
+    public float maxPower = 1000;
+    public bool heightFixed = false;
+    public bool drawPath_Debug = false;
+    public bool drawVelocity_Debug = false;
+    public bool drawPath = false;
+
+    public bool showSensor;
+    private Leader m_leader;
+    private Sensor m_sensor;
+    private float m_sensorRadius;
+    private Rigidbody m_body;
+    private Vector3 m_lastPosition;
+    private LineRenderer m_line;
+    private Vector3 smoothTrailPoint;
+
+
+    private float rndOffsetAcceleration;
+
+    private void Start () {
+        m_line = GetComponent<LineRenderer>();
+        m_leader = FindObjectOfType<Leader>();
+        m_sensor = GetComponentInChildren<Sensor>();
+        m_sensorRadius = m_sensor.GetComponent<SphereCollider>().radius * m_sensor.transform.localScale.x;
+        m_body = GetComponent<Rigidbody>();
+
+
+        m_lastPosition = smoothTrailPoint = transform.position;
+
+        if (!showSensor)
+            m_sensor.GetComponent<Renderer>().enabled = false;
+        rndOffsetAcceleration = Random.Range(0.01f, 10.0f);
+
+        FixHeight(heightFixed);
+
+        StartCoroutine(UpdateWithFixedHz(10));
+	}
+    private void Update() {
+        if (drawVelocity_Debug)
+            Debug.DrawRay(transform.position, m_body.velocity / 5, Color.black);
+        if (drawPath_Debug) {
+            Debug.DrawLine(transform.position, m_lastPosition, Color.gray, 0.66f);
+        }
+        if (drawPath) {
+            UpdateLineRenerer();
+        }
+        m_lastPosition = transform.position;
+    }
+
+    private IEnumerator UpdateWithFixedHz(float hz) {
+        float dt = 1 / hz;
+
+        float neighborAvoidPrw = 2; // Math.Pow(..., neighborAvoidPrw)
+        float neighborAvoidMult = -0.03f;
+        if (heightFixed) {
+            neighborAvoidPrw = 4;
+            neighborAvoidMult = -0.01f;
+        }
+
+    yield return new WaitUntil(() => m_sensor.CloseEntities != null);
+        while (true) {
+            float acc = 0;
+            Vector3 dir = Vector3.zero;
+
+            foreach (Transform item in m_sensor.CloseEntities) {
+                Vector3 dirFollowerers = item.transform.position - transform.position;
+                if (dirFollowerers.magnitude < (2 * m_sensorRadius)) {
+                    dir += dirFollowerers.normalized * Mathf.Pow((dirFollowerers.magnitude - 2 * m_sensorRadius), neighborAvoidPrw) * neighborAvoidMult;
+                }
+            }
+            dir += (m_leader.transform.position - transform.position).normalized;
+            acc += (m_leader.transform.position - transform.position).magnitude * 25;
+
+            m_body.AddForce(dir * Mathf.Min(acc, maxPower) * dt + Vector3.one * rndOffsetAcceleration);
+            currentSpeed = m_body.velocity.magnitude;
+
+            yield return new WaitForSeconds(dt);
+        }
+    }
+
+    private void UpdateLineRenerer() {
+        smoothTrailPoint = Vector3.Lerp(smoothTrailPoint, m_lastPosition + (transform.position - m_lastPosition), Time.deltaTime * 5);
+        m_line.SetPosition(0, transform.position);
+        m_line.SetPosition(1,  smoothTrailPoint);
+
+    }
+
+    public void FixHeight(bool fix) {
+        if (fix) {
+            heightFixed = true;
+            m_body.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY;
+        }
+    }
+    public void EnablePathDrawing(bool draw) {
+        drawPath = draw;
+        m_line.enabled = drawPath;
+    }
+    public void EnableSensorRangeDrawing(bool draw) {
+        showSensor = draw;
+        m_sensor.GetComponent<Renderer>().enabled = showSensor;
+    }
+}
